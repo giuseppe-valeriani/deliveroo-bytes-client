56828cceb4b50be5ea9d8ab9dacae3e4460527ba
Jack Tomlinson <jtommo66@hotmail.com>
2024-01-26 12:35:40 +0000

    add single item page with styling and placeholder text/images

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./styles/App.scss";
 import Header from "./components/Header/Header";
 import Home from "./pages/Home/Home";
 import WishList from "./pages/WishList/WishList";
+import SingleItem from "./components/SingleItem/SingleItem";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/name/:pageName" element={<WishList />} />
+        <Route path="/gifts/:id" element={<SingleItem />} />
       </Routes>
     </BrowserRouter>
   );
