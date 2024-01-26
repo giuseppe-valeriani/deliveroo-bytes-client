1cf8cd4dcdc0d643fc3edd57fb6db44a7c2f670c
Jack Tomlinson <jtommo66@hotmail.com>
2024-01-26 13:44:54 +0000

    add wishlist style and functionality

--- src/pages/WishList/WishList.jsx
+++ src/pages/WishList/WishList.jsx
@@ -1,11 +1,52 @@
 import "./WishList.scss";
-import { useParams } from "react-router-dom";
-
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import backArrow from "../../assets/icons/Back Button.svg";
+import search from "../../assets/icons/search.png";
+const API_URL = process.env.REACT_APP_API_URL;
+const PORT = process.env.REACT_APP_PORT;
 const WishList = () => {
   const params = useParams();
-  console.log(params);
-
-  return <h1>{params.pageName} Wish List</h1>;
+  const [gifts, setGifts] = useState(null);
+  const getGifts = async () => {
+    const response = await axios.get(`${API_URL}${PORT}/gifts`);
+    setGifts(response.data);
+  };
+  useEffect(() => {
+    getGifts();
+  }, []);
+  console.log(gifts);
+  return (
+    <main className="main">
+      <div className="wishlist-title">
+        <Link to="/">
+          <img src={backArrow} alt="back arrow" className="icon" />
+        </Link>
+        <h3>{params.pageName} Wish List</h3>
+        <img src={search} alt="Magnifying glass" className="icon" />
+      </div>
+      <div className="gift-cards">
+        <div className="gift-card">
+          {gifts &&
+            gifts.map((gift) => {
+              return (
+                <Link to="/">
+                  <div className="card" key={gift.id}>
+                    <img
+                      className="gift-image"
+                      src={gift.image}
+                      alt="Gift item"
+                    />
+                    <h4>{gift.gift}</h4>
+                    <h4>{gift.price}</h4>
+                  </div>
+                </Link>
+              );
+            })}
+        </div>
+      </div>
+    </main>
+  );
 };
-
 export default WishList;
